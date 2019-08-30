4fa582cdbdf4b4af79a72ab9b0b0904b84adb876
Remi Caroff <caroffremi@msn.com>
2019-08-30 17:59:47 +0200

    added Message models for mailjet

---
+++ DrawingOfLot/Messages.swift
@@ -0,0 +1,64 @@
+//
+//  Messages.swift
+//  DrawingOfLot
+//
+//  Created by Rémi Caroff on 30/08/2019.
+//  Copyright © 2019 Rémi Caroff. All rights reserved.
+//
+
+/*
+ curl -s \
+ -X POST \
+ --user "$MJ_APIKEY_PUBLIC:$MJ_APIKEY_PRIVATE" \
+ https://api.mailjet.com/v3.1/send \
+ -H 'Content-Type: application/json' \
+ -d '{
+   "Messages":[
+     {
+       "From": {
+         "Email": "remi.caroff@link-value.fr",
+         "Name": "Rémi"
+       },
+       "To": [
+         {
+           "Email": "passenger1@example.com",
+           "Name": "passenger 1"
+         }
+       ],
+       "TemplateID": 980806,
+       "TemplateLanguage": true,
+       "Subject": "Secret Santa !",
+       "Variables": {
+   "name": "",
+   "receiver": "PERSONNE"
+   }
+     }
+   ]
+ }'
+ */
+
+import Foundation
+
+class MessageList: Encodable {
+  var SandboxMode: Bool = true
+  var Messages: [Message] = []
+}
+
+class Message: Encodable {
+  var From: MailContact = MailContact(Email: "remi.caroff@link-value.fr", Name: "Rémi Caroff")
+  var To: [MailContact] = []
+  let TemplateID: String = "980806"
+  var TemplateLanguage: Bool = true
+  var Subject: String = "Secret Santa chez les Caroff !"
+  var Variables: MailVariables = MailVariables()
+}
+
+struct MailContact: Encodable {
+  var Email: String
+  var Name: String
+}
+
+struct MailVariables: Encodable {
+  var name: String = ""
+  var receiver: String = ""
+}
